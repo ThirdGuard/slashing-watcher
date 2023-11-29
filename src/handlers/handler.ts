b379362d1b58a1c249a2cdad93b0684f8ec66d05
Pri Patel <pripatel93@gmail.com>
2023-11-29 03:18:50 +0000

    clean up

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,6 +1,3 @@
-// import { FullBlockInfo } from './src/providers/consensus/typings'; // Adjust import paths as necessary
-// import { AlertBody } from './src/providers/alertmanager/typings'; // Adjust import paths as necessary
-
 const KEEP_MAX_SENT_ALERTS = 10;
 
 type AlertBody = any;
@@ -13,8 +10,6 @@ export abstract class WatcherHandler {
         this.sentAlerts = [];
     }
 
-    // The '@unsync' decorator needs to be handled according to your project's async handling strategy.
-    // If it is just to denote that the method is asynchronous, then you can use async keyword in TypeScript.
     async handle(watcher: any, head: FullBlockInfo): Promise<void> {
 
     };
